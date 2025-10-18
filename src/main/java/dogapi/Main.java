c433e12ca593eca44918f2be4505c8e94c4a1c16
guess <layurraa0831@gmail.com>
2025-10-17 22:47:39 -0400

    update docstring

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -26,6 +26,7 @@ public class Main {
     public static int getNumberOfSubBreeds(String breed, BreedFetcher breedFetcher) {
         // Task 3 implement this code so that it is entirely consistent with its provided documentation.
         // return statement included so that the starter code can compile and run.
+        // +++
         try {
             List<String> subBreeds = breedFetcher.getSubBreeds(breed);
             if (subBreeds == null) {
